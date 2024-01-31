5169df95b4136f19ae9717ddaee8da82bf94f307
pipocadr <pipocadr@gmail.com>
2024-01-31 13:53:58 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: join(__dirname, "../../"),
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
